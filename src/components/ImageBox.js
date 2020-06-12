a433ef1fc4df41c58991431ca785b7c48efb4be6
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-12 11:53:25 -0500

    scaffold image box component

---
+++ src/components/ImageBox.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const ImageBox = (props) => {
+
+    const key = "image-box-" + props.imageKey;
+    const divStyle = {
+        backgroundImage: "url('" + props.url + "')",
+        height: props.height,
+        width: props.width
+      }
+    const label = "Box " + props.imageKey;
+  
+    return(
+        <React.Fragment >
+            <div key={key} className="col-lg-3 col-md-6 col-sm-12">
+              <div className="box m-1 p-1" style={divStyle}>{label}</div>
+            </div>
+        </React.Fragment>
+    );
+  
+  }
--- src/pages/home/Component.js
+++ src/pages/home/Component.js
@@ -3,9 +3,9 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as Actions from '../../redux/ActionCreators';
 
-import logo from './logo.svg';
 
 import './styles.css';
+import {ImageBox} from '../../components/ImageBox';
 
 const mapStateToProps = state => ({
     ...state
@@ -22,21 +22,52 @@ class Home extends Component {
 
     render() {
         return(
-            <div className="App">
-            <header className="App-header">
-              <img src={logo} className="App-logo" alt="logo" />
-              <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-              <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Learn React
-              </a>
-            </header>
+            <div className="home-page mt-5 pt-5">
+
+              <div className="row m-2 p-2 text-center">
+                <ImageBox 
+                  imageKey="1" 
+                  url="https://cdn.fotomashup.com/2020/06/10214318/y9bfvi6080121.jpg"
+                  height = "125px"
+                  width = "125px"
+                 />
+                
+                <ImageBox 
+                  imageKey="2" 
+                  url="https://cdn.fotomashup.com/2020/06/10214304/xf366110106c3d40f98d8dbbe7decab0a-700.jpg" 
+                  height = "125px"
+                  width = "125px"
+                />
+                
+                <ImageBox 
+                  imageKey="3" 
+                  url="https://cdn.fotomashup.com/2020/06/10214241/Wonderland-004.jpg" 
+                  height = "125px"
+                  width = "125px"
+                />
+
+                <ImageBox 
+                  imageKey="4" 
+                  url="https://cdn.fotomashup.com/2020/06/10214226/women-looking-at-viewer-stockings-black-hair-kitchen-boobs-lingerie-clothing-French-Maid-Leah-Francis-lady-leg-costume-undergarment-fetish-model-soubrette-339304.jpg" 
+                  height = "125px"
+                  width = "125px"
+                />
+
+                <ImageBox 
+                  imageKey="5" 
+                  url="https://cdn.fotomashup.com/2020/06/10214231/women-model-black-hair-leather-latex-catsuit-323011-wallhere.com_.jpg" 
+                  height = "125px"
+                  width = "125px"
+                />
+
+                <ImageBox 
+                  imageKey="6" 
+                  url="https://cdn.fotomashup.com/2020/06/10214206/women-1591617702767-5078.jpg" 
+                  height = "125px"
+                  width = "125px"
+                />
+              </div>
+
           </div>
         );
     
@@ -44,3 +75,4 @@ class Home extends Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
+
