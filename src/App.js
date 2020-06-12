e81a42ac8a138c79f31e6e33c84965fc45bf542d
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-12 18:01:59 -0500

    lint unused stock files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Main from './components/Main';
-import './App.css';
+
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
