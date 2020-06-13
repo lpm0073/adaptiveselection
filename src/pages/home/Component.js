d2a283b7744c962b88d3a38783da0935a91ac692
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-12 19:48:36 -0500

    working draft of randomized image selection.

--- src/pages/home/Component.js
+++ src/pages/home/Component.js
@@ -27,17 +27,118 @@ class Home extends Component {
       isCategoryInitialized: false,
       fetcherTimeout: null,
       level: 1,
+      page_number: 1,
       category_exclusions: [],
       media_query: '',
-      image_working_set: null,
-      image_queue: null,
-      page_number: 1
+      image_working_set: [],
+      image_carousel: [],
+      number_of_images: 10
     }
 
-    this.fetcher = this.fetcher.bind(this);
-    this.handle_level = this.handle_level.bind(this);
+    this.imageFetcher = this.imageFetcher.bind(this);
+    this.handleChangeLevel = this.handleChangeLevel.bind(this);
+    this.repaint = this.repaint.bind(this);
+    this.getRandomImageFrame = this.getRandomImageFrame.bind(this);
+    this.isImageCollision = this.isImageCollision.bind(this);
+    this.getRandomImage = this.getRandomImage.bind(this);
+    this.setBackgroundUrl = this.setBackgroundUrl.bind(this);
+    this.getElapsedTime = this.getElapsedTime.bind(this);
+  }
+
+  getRandomImageFrame() {
+    console.log("getRandomImageFrame");
+    return Math.floor(Math.random() * this.state.number_of_images);
+  }
+
+  isImageCollision(url) {
+    console.log("isImageCollision");
+    this.state.image_carousel.map((image) => {
+        if (url === image.source_url) return true;
+    });
+    return false;
+
+  }
+
+  getRandomImage(images) {
+    console.log("getRandomImage()");
+
+    if (images === null) return null;
+
+    var image, i = 0;
+    do {
+        image = images[Math.floor(Math.random() * images.length)];
+        i++;
+    } while (this.isImageCollision(image) && (i <= images.length))
+    return image;
+  }
+
+  getElapsedTime(imageKey) {
+    const d = new Date();
+
+    this.state.image_carousel.map((image) => {
+        if (image.key === imageKey) return d - image.timestamp;
+    });
+    return 999999;
+  }
+
+  setBackgroundUrl(imageKey, newImage) {
+    console.log("setBackgroundUrl() - imageKey", imageKey);
+    console.log("setBackgroundUrl() - newImage", newImage);
+
+    newImage = wpGetImage(newImage, "medium");
+    console.log("setBackgroundUrl() - newImage (processed)", newImage);
+
+    var newImageSet = [];
+    const obj = {
+      key: imageKey,
+      source_url: newImage.source_url,
+      height: newImage.height,
+      width: newImage.width,
+      timestamp: new Date()
+    }
+    newImageSet.push(obj);
+
+    this.state.image_carousel.map((image) => {
+        if (image.key !== imageKey.key) {
+          newImageSet.push(image);
+        }
+    });
+
+    console.log("setBackgroundUrl() - newImageSet", newImageSet);
+
+    this.setState({
+      image_carousel: newImageSet
+    });
 
 
+  }
+
+  repaint() {
+    /* place a random image on a random imageKey at a random point in time. */
+    if (this.state.image_working_set.length > 0) {
+
+        const imageKey = this.getRandomImageFrame();
+        const images = this.state.image_working_set;
+
+        const image = this.getRandomImage(images);
+        const elapsed = this.getElapsedTime(imageKey);
+
+        console.log("repaint() imageKey", imageKey);
+        console.log("repaint() images", images);
+        console.log("repaint() image", image);
+        console.log("repaint() elapsed", elapsed);
+
+        if (imageKey != null && elapsed > 2000) {
+            this.setBackgroundUrl(imageKey, image);
+        }
+
+    }
+
+    const self = this;
+    setTimeout(function() {
+        self.repaint();
+    }, 5000 * Math.random());   
+  
   }
 
   shouldComponentUpdate(nextProps, nextState) {
@@ -51,18 +152,30 @@ class Home extends Component {
 
         const self = this;
         const fetcherTimeout = setTimeout(function() {
-            self.handle_level();
-            self.fetcher();
+            self.handleChangeLevel();
+            self.imageFetcher();
+            self.repaint();
         }, 250);    
         this.setState({
           fetcherTimeout: fetcherTimeout
         });
-
   }
 
-  handle_level() {
+  componentWillUnmount() {
+    clearTimeout(this.state.fetcherTimeout);
+  }
 
-    if (!this.props.categories.isLoading) {
+  handleChangeLevel() {
+
+    if (this.props.categories.isLoading && !this.state.isCategoryInitialized) {
+      // we're not ready, so wait 500ms and then try again.
+      const self = this;
+      setTimeout(function() {
+          self.handleChangeLevel();
+      }, 500);
+      return;
+    }
+
       const cats = this.props.categories.items;
       const exclusions = wpGetExclusions(this.state.level, cats);
       this.setState({
@@ -71,11 +184,9 @@ class Home extends Component {
         isCategoryInitialized: true
       });
   
-    }
-  
   }
 
-  fetcher() {
+  imageFetcher() {
 
     // initialize the state media_query URL
     if (this.state.isCategoryInitialized && !this.props.categories.isLoading) {
@@ -98,14 +209,19 @@ class Home extends Component {
           })
       .then(response => response.json())
       .then(images => {
+        const image_working_set = this.state.image_working_set.concat(images);
+        console.log("loaded: image_working_set", image_working_set);
         this.setState({
-          image_queue: images,
+          image_working_set: image_working_set,
           page_number: this.state.page_number + 1
         });
-        console.log(images);
       })
       .catch(error => {
-        /*
+        console.log("frowny face.");
+        this.setState({
+          page_number: 1
+        });
+         /*
             {
               "code": "rest_post_invalid_page_number",
               "message": "The page number requested is larger than the number of pages available.",
@@ -113,20 +229,14 @@ class Home extends Component {
                 "status": 400
                 }
             }
-
         */
-       console.log("frowny face.");
-       this.setState({
-         page_number: 0
-       });
-
       });
   
     }
 
     const self = this;
     setTimeout(function() {
-        self.fetcher();
+        self.imageFetcher();
     }, 30000 * Math.random());   
 
   }
@@ -135,15 +245,15 @@ class Home extends Component {
       return(
           <div className="home-page mt-5 pt-5">
             <div className="row m-2 p-2 text-center">
-              {this.state.image_queue != null ? this.state.image_queue.map((obj, indx) => {
-                  var imgDict = wpGetImage(obj, "medium");
+              {this.state.image_carousel.length > 0 ? this.state.image_carousel.map((image, indx) => {
+                  console.log("render()", image);
                   return (
                     <ImageBox 
                     key={indx}
                     imageKey={indx}
-                    url={imgDict.source_url}
-                    height = {imgDict.height}
-                    width = {imgDict.width}
+                    url={image.source_url}
+                    height = {image.height}
+                    width = {image.width}
                     />
                   );
                 })
@@ -155,6 +265,9 @@ class Home extends Component {
       );
   
   }
+
+
+
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
