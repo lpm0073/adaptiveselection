240e0001a91a158f0f0a3b2607b4763f58228111
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-18 08:49:54 -0500

    switch to 10-count results in image api

--- src/shared/urls.js
+++ src/shared/urls.js
@@ -1,5 +1,5 @@
 export const baseUrl = 'http://localhost:3001/';
 export const legalUrl = 'https://google.com';
 export const backendUrl = 'https://api.fotomashup.com/wp-json/wp/v2/';
-export const mediaUrl = backendUrl + 'media?_fields=id,categories,acf,media_details&per_page=4';
+export const mediaUrl = backendUrl + 'media?_fields=id,categories,acf,media_details&per_page=10';
 export const categoriesUrl = backendUrl + 'categories?per_page=100&_fields=id,count,acf';
