7a01c6625b888fe27ca62b075828d5690396d155
Lawrence McDaniel <lpm0073@gmail.com>
2020-06-29 18:00:05 -0500

    build ArticleBox content/styling

--- src/components/articleBox/ArticleBox.js
+++ src/components/articleBox/ArticleBox.js
@@ -22,11 +22,15 @@ const mapStateToProps = state => ({
           const style={
               height: this.props.height
           };
-          var cls = "m-0 p-0 ";
-          if (this.props.columns > 2) cls += " article-box"
+          var cls = "m-0 p-0 article-box";
+          if (this.props.columns < 3) cls += " hidden"
           return(
             <div className={"m-0 p-1 article-container " + this.props.className}>
-              <div className={cls} style={style}></div>
+              <div className={cls} style={style}>
+                <h3 className="heading text-center pt-2 pb-0">Headline</h3>
+                <h5 className="sub-heading text-center pt-0 pb-0 mb-0">Sub-Headline</h5>
+                <div className="article text-justify mt-0 pt-0 p-3">Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32. There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</div>
+              </div>
             </div>
         );
       }
